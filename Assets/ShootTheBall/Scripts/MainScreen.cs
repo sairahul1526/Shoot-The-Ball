b1149514d34b8f3658eb8d522254bd81e265fd6c
sairahul1526 <dravid.rahul1526@gmail.com>
2018-02-11 12:44:50 +0530

    Initial commit

---
+++ Assets/ShootTheBall/Scripts/MainScreen.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainScreen : MonoBehaviour {
+
+	public Text txtBest;
+
+	/// <summary>
+	/// Raises the enable event.
+	/// </summary>
+	void OnEnable()
+	{
+		txtBest.text = "BEST : " + PlayerPrefs.GetInt ("BestScore", 0).ToString("00");
+	}
+
+	int start = 0;
+
+	void Update() {
+		if (start == 0) {
+			if (Input.GetButtonDown("Submit")) {
+				InputManager.instance.DisableTouchForDelay ();
+				InputManager.instance.AddButtonTouchEffect ();
+				GameController.instance.StartGamePlay(gameObject);
+				start = 1;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Raises the play button pressed event.
+	/// </summary>
+	public void OnPlayButtonPressed()
+	{
+		if (Input.GetButtonDown("Submit")) {
+			InputManager.instance.DisableTouchForDelay ();
+			InputManager.instance.AddButtonTouchEffect ();
+			GameController.instance.StartGamePlay(gameObject);
+		}
+	}
+
+	public void OnReviewButtonPressed()
+	{
+		if (InputManager.instance.canInput ()) {
+			InputManager.instance.DisableTouchForDelay ();
+			InputManager.instance.AddButtonTouchEffect ();
+			Application.OpenURL(Constants.REVIEW_URL);
+		}
+	}
+
+}
---
+++ Assets/ShootTheBall/Scripts/UnityAds.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+using System;
+
+
+public class UnityAds : MonoBehaviour 
+{
+	#if UNITY_ANDROID || UNITY_IOS
+	public static UnityAds instance;
+	public string androidAppID;
+	public string iOSAppID;
+
+	void Awake()
+	{
+		if (instance != null) 
+		{
+			Destroy(gameObject);
+			return;
+		}
+		instance = this;
+	}
+
+	public void init(bool enableTestMode = false)
+	{
+		if(!Advertisement.isInitialized)
+		{
+			#if UNITY_ANDROID
+			Advertisement.Initialize (androidAppID, enableTestMode);	
+			#elif UNITY_IOS
+			Advertisement.Initialize (iOSAppID);
+			#endif
+		}
+	}
+
+	public void init(string androidAppId, string iosAppId, bool enableTestMode = false)
+	{
+		if (!Advertisement.isInitialized) {
+			#if UNITY_ANDROID
+			Advertisement.Initialize (androidAppId, enableTestMode);	
+			#elif UNITY_IOS
+			Advertisement.Initialize (iosAppId, enableTestMode);
+			#endif
+		}
+	}
+
+	public bool IsReady()
+	{
+		return Advertisement.IsReady ();
+	}
+
+	public bool IsReady(string adZone)
+	{
+		return Advertisement.IsReady (adZone);
+	}
+
+	public bool isShowing()
+	{
+		return Advertisement.isShowing;
+	}
+
+	public bool isSupported()
+	{
+		return Advertisement.isSupported;
+	}
+
+	public string getGameID()
+	{
+		return Advertisement.gameId;
+	}
+
+	public void SetCampaignDataURL(string URL)
+	{
+		Advertisement.SetCampaignDataURL (URL);
+	}
+
+	public void ShowAds()
+	{
+		if (Advertisement.IsReady ()) {
+			Advertisement.Show ();
+		}
+	}
+
+	public void ShowAds(string zoneId)
+	{
+		if (Advertisement.IsReady (zoneId)) {
+			Advertisement.Show (zoneId);
+		}
+	}
+
+	public void ShowAds(string zoneId, ShowOptions options)
+	{
+		if (Advertisement.IsReady (zoneId)) {
+			Advertisement.Show (zoneId, options);
+		}
+	}
+
+	public void ShowAds(string zoneId, Action<ShowResult> adResult)
+	{
+		if (Advertisement.IsReady (zoneId)) {
+			Advertisement.Show( zoneId, new ShowOptions {
+				resultCallback = result => {
+					adResult (result);
+				}
+			});
+		} else {
+			adResult (ShowResult.Failed);
+		}
+	}
+
+	public void ShowAdsWithResult(string zoneId, Action<bool> adResult)
+	{
+		if (Advertisement.IsReady (zoneId)) {
+			Advertisement.Show( zoneId, new ShowOptions {
+				resultCallback = result => {
+					adResult ((result == ShowResult.Finished) ? true : false);
+				}
+			});
+		} else {
+			adResult (false);
+		}
+	}
+	#endif
+}
